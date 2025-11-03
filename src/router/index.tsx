b9cb7c0b516cdded5a36106c675491755f3ebbc5
mygo <1158270624@qq.com>
2025-11-03 22:58:47 +0800

    🎉 初次提交

---
+++ src/router/index.tsx
@@ -0,0 +1,22 @@
+// 定义路由
+import { createBrowserRouter } from "react-router"
+import { Board } from "@/view/Borad"
+
+// 先定义路由
+const routes = [
+{
+        path: '/',
+        element: <div>首页</div>
+},
+{
+        path: '/about',
+        element: <Board />
+}
+]
+
+
+// createBrowserRouter  创建路由实例
+// createHashRouter  创建路由实例，使用hash模式
+// createMemoryRouter  创建路由实例，使用内存模式
+// createStaticRouter  创建路由实例，使用静态模式
+export const router = createBrowserRouter(routes)
