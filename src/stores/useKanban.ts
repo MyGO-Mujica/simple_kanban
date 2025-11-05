2cc3adb4b7c9144bd3821d4538e38efef6c84dca
mygo <1158270624@qq.com>
2025-11-05 17:51:32 +0800

    ✅ feat：新增updateBoard方法，实现新建任务效果

--- src/stores/useKanban.ts
+++ src/stores/useKanban.ts
@@ -1,15 +1,25 @@
 import { create } from "zustand"
 
+type Task = {
+  id: number | string
+  title: string
+}
+
 interface Board {
-  id: string
-  name: string
+  groupId: string | number
+  groupName: string
+  tasks: Task[]
 }
 export const useKanban = create<{
   boards: Board[];
   createBoard:(board: Board) =>void
+  updateBoard:(board: Board) =>void
 }>((set) =>( {
     boards: [],
     createBoard: (board :Board) => set((state) => ({
         boards: [...state.boards, board]
+    })),
+    updateBoard: (board: Board) => set((state) => ({
+        boards: state.boards.map(b => b.groupId === board.groupId ? board : b)
     }))
 }))
