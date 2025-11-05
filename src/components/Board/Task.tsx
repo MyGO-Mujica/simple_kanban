52661849106ebb2a43926b26a01d403d592dcb11
mygo <1158270624@qq.com>
2025-11-05 20:34:32 +0800

    ✅ feat: 实现 Task 组件可拖动功能

--- src/components/Board/Task.tsx
+++ src/components/Board/Task.tsx
@@ -1,10 +1,32 @@
+import { useDraggable } from "@dnd-kit/core";
+
 interface TaskProps {
+  id: number | string;
   title: string
 }
 export const Task = (props:TaskProps) => {
-  const { title } = props
+  const { id, title } = props;
+  // ← 把这个组件变成可拖动的
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+     id,  // ← 任务的唯一 id（时间戳）
+     data: {
+        type: 'task', // ← 标记这是一个任务（非看板）
+     }
+  });
+const style :React.CSSProperties | undefined =
+      transform ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      } : undefined
+
+
   return (
-    <div className="p-2 mb-1 bg-white border-gray-100 rounded-md">
+    <div 
+       ref={setNodeRef} 
+        {...listeners}
+        {...attributes}
+       className="p-2 mb-1 bg-white border-gray-100 rounded-md"
+       style={style}
+    >
       {title}
     </div>
   )
