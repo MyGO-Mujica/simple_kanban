984e444b42e520e122c3a439d5a68da69e5839e0
mygo <1158270624@qq.com>
2025-11-05 20:26:11 +0800

    ✅ feat: 添加拖放功能和优化 Kanban 列容器

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,24 +1,43 @@
 import { useKanban } from "@/stores/useKanban";
 import { Task } from "./Task";
-export const Borad =() => {
-  const { boards } = useKanban(state => state);
+import { DndContext } from "@dnd-kit/core";
+import { KanbanGroup } from "./KanbanGroup";
+import { Button }from "@/components/ui/button";
+
+export const Board =() => {
+  const { boards, updateBoard} = useKanban(state => state);
   return (
+     /* ← dnd-kit 的根容器 */
+    <DndContext>
       <div className="flex flex-row">
         {boards.map((board) => (
-          <div key={board.id} className="flex flex-row">
-            <div className="kanban-group p-2 mr-2 w-[260px] rounded-3xl bg-blue-100">
-              <div className="flex flex-col ">
-                 <div className="w-fit rounded-full bg-blue-300 px-1 mb-2">未开始</div>
-                   <div>
+          <div key={board.groupId} className="flex flex-row">
+            <KanbanGroup  groupId={board.groupId} title={board.groupName} >
+              <div className="kanban-group p-2 mr-2 w-[260px] rounded-3xl bg-blue-100">
+                <div className="flex flex-col ">
+                   <div className="w-fit rounded-full bg-blue-300 px-1 mb-2">未开始</div>
+                     <div>
                           {
-                           [0,1,2].map(item =>(
-                             <Task key={item} title={`任务${item}`} />
+                           board.tasks.map((item, index) =>(
+                             <Task 
+                             key={item.id}  
+                             id={item.id}
+                             title={`任务${index + 1}`} />
                           ))}
-                   </div>
+                     </div>
+                  </div>
                </div>
-             </div>
+                <Button 
+                 onClick={() => updateBoard({ 
+                 groupId: board.groupId,
+                 groupName: board.groupName,
+                 tasks: [...board.tasks, { id: Date.now(), title: '新任务' }] })}>
+                  新建任务
+                 </Button>
+             </KanbanGroup>
            </div>
         ))}
       </div>
+    </DndContext>
   )
 }
